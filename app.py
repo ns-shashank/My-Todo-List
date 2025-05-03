155599f4d08e84bf6babcc1e47c39541875fcdbb
ns-shashank <nsshashankl@gmail.com>
2025-05-03 16:52:18 +0530

    Edited

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import os
 
 app = Flask(__name__)
 TODO_FILE = "todo.txt"
@@ -42,4 +43,6 @@ def delete(index):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
